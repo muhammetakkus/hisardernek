28fdaa354e0ba831a0ef0d1a7ccb191b3fefbb5d
M.Mehmet Akkuş <muhammetakkuss34@gmail.com>
2020-04-16 00:56:17 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,384 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="site-wrap">
+        <div class="site-navbar mt-4">
+          <div class="container py-1">
+            <div class="row align-items-center">
+              <div class="col-8 col-md-8 col-lg-4">
+                <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0"><strong>Hisar<span class="text-primary"> </span></strong></a></h1>
+              </div>
+              <div class="col-4 col-md-4 col-lg-8">
+                <nav class="site-navigation text-right text-md-right" role="navigation">
+
+                  <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+                  <ul class="site-menu js-clone-nav d-none d-lg-block">
+                    <li>
+                      <a href="index.html">Home</a>
+                    </li>
+                    <li class="active"><a href="about.html">About</a></li>
+                    <li class="has-children">
+                      <a href="apartments.html">Apartments</a>
+                      <ul class="dropdown arrow-top">
+                        <li><a href="#">Apartments</a></li>
+                        <li><a href="#">Rooms</a></li>
+                        <li><a href="#">Suites</a></li>
+                        <li class="has-children">
+                          <a href="#">Sub Menu</a>
+                          <ul class="dropdown">
+                            <li><a href="#">Menu One</a></li>
+                            <li><a href="#">Menu Two</a></li>
+                            <li><a href="#">Menu Three</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                    <li><a href="news.html">News</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-mobile-menu">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+       <!--  -->
+      <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url('images/hero_bg_1.jpg');" data-stellar-background-ratio="0.5" data-aos="fade">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7 text-center" data-aos="fade-up" data-aos-delay="400">
+              <h2 class="text-white">Hisar Eğitim ve Kültüre Hizmet Derneği</h2>
+              <p>Mustafa Şahin</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-lg-6">
+            <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
+            <h2 class="mb-5">Our Office</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, fugit nam obcaecati fuga itaque deserunt
+              officia, error reiciendis ab quod?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <div class="site-section">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
+            <h2 class="mb-5">Team</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, fugit nam obcaecati fuga itaque deserunt
+              officia, error reiciendis ab quod?</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/person_1.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Megan Smith</h2>
+                <span class="d-block mb-2 text-white-opacity-05">Co Founder</span>
+                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit ullam reprehenderit nemo.</p>
+                <p>
+                  <a href="#" class="text-white p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-linkedin"></span></a>
+                </p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Brooke Cagle</h2>
+                <span class="d-block mb-2 text-white-opacity-05">Co Founder</span>
+                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit ullam reprehenderit nemo.</p>
+                <p>
+                  <a href="#" class="text-white p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-linkedin"></span></a>
+                </p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/person_3.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Philip Martin</h2>
+                <span class="d-block mb-2 text-white-opacity-05">Co Founder</span>
+                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit ullam reprehenderit nemo.</p>
+                <p>
+                  <a href="#" class="text-white p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-linkedin"></span></a>
+                </p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/person_4.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Steven Ericson</h2>
+                <span class="d-block mb-2 text-white-opacity-05">Co Founder</span>
+                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit ullam reprehenderit nemo.</p>
+                <p>
+                  <a href="#" class="text-white p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-linkedin"></span></a>
+                </p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/person_1.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Nathan Dumlao</h2>
+                <span class="d-block mb-2 text-white-opacity-05">Co Founder</span>
+                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit ullam reprehenderit nemo.</p>
+                <p>
+                  <a href="#" class="text-white p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-linkedin"></span></a>
+                </p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Brooke Cagle</h2>
+                <span class="d-block mb-2 text-white-opacity-05">Co Founder</span>
+                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit ullam reprehenderit nemo.</p>
+                <p>
+                  <a href="#" class="text-white p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-linkedin"></span></a>
+                </p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <div class="site-section">
+        <div class="container">
+
+          <div class="row">
+            <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto" data-aos="fade-up">
+              <h2 class="mb-5">Featured Apartments</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, fugit nam obcaecati fuga itaque deserunt officia, error reiciendis ab quod?</p>
+            </div>
+          </div>
+
+          <div class="site-block-retro d-block d-md-flex">
+
+            <a href="#" class="col1 unit-9 no-height" data-aos="fade-up" data-aos-delay="100">
+              <div class="image" style="background-image: url('images/img_2.jpg');"></div>
+              <div class="unit-9-content">
+                <h2>Baltimore Apartment</h2>
+                <span>$600/night</span>
+              </div>
+            </a>
+
+            <div class="col2 ml-auto">
+
+              <a href="#" class="col2-row1 unit-9 no-height" data-aos="fade-up" data-aos-delay="200">
+                <div class="image" style="background-image: url('images/img_3.jpg');"></div>
+                <div class="unit-9-content">
+                  <h2>Austin Apartment</h2>
+                  <span>$290/night</span>
+                </div>
+              </a>
+
+              <a href="#" class="col2-row2 unit-9 no-height" data-aos="fade-up" data-aos-delay="300">
+                <div class="image" style="background-image: url('images/img_1.jpg');"></div>
+                <div class="unit-9-content">
+                  <h2>Atlanta Apartment</h2>
+                  <span>$1,290/night</span>
+                </div>
+              </a>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+
+
+      <div class="bg-primary" data-aos="fade">
+        <div class="container">
+          <div class="row">
+            <p>test</p>
+          </div>
+        </div>
+      </div>
+
+      <footer class="site-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="mb-5">
+                <h3 class="footer-heading mb-4">About Apart</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero atque, consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis blanditiis, minima minus odio!</p>
+              </div>
+              <div class="mb-5">
+                <h3 class="footer-heading mb-4">Subscribe</h3>
+                <form action="#" method="post" class="site-block-subscribe">
+                  <div class="input-group mb-3">
+                    <input type="text" class="form-control border-secondary bg-transparent" placeholder="Enter your email"
+                      aria-label="Enter Email" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary rounded-top-right-0" type="button" id="button-addon2">Subscribe</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="col-lg-4 mb-5 mb-lg-0">
+              <div class="row mb-5">
+                <div class="col-md-12">
+                  <h3 class="footer-heading mb-4">Navigations</h3>
+                </div>
+                <div class="col-md-6 col-lg-6">
+                  <ul class="list-unstyled">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Apartments</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Featured Apartment</a></li>
+                  </ul>
+                </div>
+                <div class="col-md-6 col-lg-6">
+                  <ul class="list-unstyled">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Membership</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-12">
+                  <h3 class="footer-heading mb-4">Follow Us</h3>
+
+                  <div>
+                    <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                    <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                    <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                    <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <div class="col-lg-4 mb-5 mb-lg-0">
+              <div class="mb-5">
+                <h3 class="footer-heading mb-4">Watch Live Streaming</h3>
+
+                <div class="block-16">
+                  <figure>
+                    <img src="images/img_1.jpg" alt="Image placeholder" class="img-fluid rounded">
+                    <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span class="icon-play"></span></a>
+                  </figure>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="row text-center">
+            <div class="col-md-12">
+              <p> tester xxx </p>
+            </div>
+          </div>
+
+        </div>
+      </footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style media="screen">
+   @import '../assets/fonts/icomoon/style.css';
+   @import '../assets/css/bootstrap.min.css';
+   @import '../assets/css/magnific-popup.css';
+   @import '../assets/css/jquery-ui.css';
+   @import '../assets/css/owl.carousel.min.css';
+   /* @import '../assets/owl.theme.default.min.css'; */
+   @import '../assets/css/bootstrap-datepicker.css';
+   /* @import '../assets/css/mediaelementplayer.css'; */
+
+   @import '../assets/css/animate.css';
+   @import '../assets/fonts/flaticon/font/flaticon.css';
+   @import '../assets/css/fl-bigmug-line.css';
+   @import '../assets/css/aos.css';
+   @import '../assets/css/style.css';
+
+</style>
